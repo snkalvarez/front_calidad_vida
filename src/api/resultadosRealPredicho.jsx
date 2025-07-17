249a4f006de052950cd3a365f3e53e223d8344e9
jalvarez <lcalvarez@unicauca.edu.co>
2025-07-16 23:48:26 -0500

    Update resultadosRealPredicho.jsx

--- src/api/resultadosRealPredicho.jsx
+++ src/api/resultadosRealPredicho.jsx
@@ -2,7 +2,7 @@ import apiClientCalidadVida from "./apliClient";
 
 export const obtenerResultadosRealVsPrediccion = async (modelo) => {
     try {
-        const response = await apiClientCalidadVida.get(`/data_real_predic/pkl?model=${modelo}&num_muestras=3000`);
+        const response = await apiClientCalidadVida.get(`/data_real_predic/csv?model=${modelo}&num_muestras=3000`);
         return response.data;
     } catch (error) {
         console.error("Error obteniendo los resultados reales vs predicción: ", error);
